6c903443137689559ced8d011cbf9eb6693bc388
Shtirlits1972 <shtirlits1972@gmail.com>
2021-02-14 22:00:49 +0200

    последняя версия
    последняя версия

--- vizitka1/lib/main.dart
+++ vizitka1/lib/main.dart
@@ -48,24 +48,54 @@ class VizitContainer extends StatelessWidget{
       color: Colors.white,
       
        child: Column(children: <Widget>[
-       Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEUDE6-PDdkNcAG10PcudDCjhViFuWuyPBIg&usqp=CAU', 
-       width: 180, fit: BoxFit.fitWidth),
-        // Container(child: Image.asset('assets/ava3.jpg', width: 220, height: 140, fit: BoxFit.fitWidth),),
+      //  Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEUDE6-PDdkNcAG10PcudDCjhViFuWuyPBIg&usqp=CAU', 
+      //  width: 180, fit: BoxFit.fitWidth),
+        Container( 
+          margin: const EdgeInsets.all(25.0),
+          padding: const EdgeInsets.all(3.0),
+          child: Image.asset('img/ava3.jpg', width: 220, height: 240, fit: BoxFit.fitWidth),),
 
-          Text('(096) 150-94-99)',
-              textDirection: TextDirection.ltr,
-              textAlign: TextAlign.center,  
-              style: TextStyle(color: Colors.black,   
-            fontSize: 20,                       
-            backgroundColor: Colors.white,   
-        ), ),
-          Text('shtirlits1972@gmail.com',
-                            textDirection: TextDirection.ltr,
-                            textAlign: TextAlign.center,  
-                      style: TextStyle(color: Colors.black,   
-            fontSize: 20,                       
-            backgroundColor: Colors.white,    
-        ),),
+
+Container(
+ margin: const EdgeInsets.only(bottom: 20),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+    border: Border.all(color: Colors.blueAccent, 
+    
+    width: 5.0, 
+    ),
+      color: Colors.blueGrey[100],
+  ),
+
+  child: 
+RichText(
+      textDirection: TextDirection.ltr,
+        text: TextSpan(
+          text: "       тел. (096) 150-94-99      ",
+          style: TextStyle(color: Colors.black, fontSize: 20,
+      )
+     ),
+),),
+
+Container(
+
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+    border: Border.all(color: Colors.blueAccent, 
+    
+    width: 5.0, 
+    ),
+    color: Colors.grey[300],
+  ),
+  child: 
+RichText(
+      textDirection: TextDirection.ltr,
+        text: TextSpan(
+          text: "  shtirlits1972@gmail.com   ",
+          style: TextStyle(color: Colors.black, fontSize: 20,
+      )
+     ),
+),),
 
        ],
                crossAxisAlignment: CrossAxisAlignment.start,
